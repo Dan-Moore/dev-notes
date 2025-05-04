bef677edd9523bb265a87c216555dfce50f49438
Dan Moore <grep.daniel@gmail.com>
2025-05-04 19:51:33 -0400

    import cleanup

--- app/learning/page.tsx
+++ app/learning/page.tsx
@@ -1,40 +1,15 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import { DataTable, DataTableDemo } from "./data-table"
-import { SectionCards } from "@/components/section-cards"
+import { DataTable } from "./data-table"
 import { SiteHeader } from "@/components/site-header"
 import {
   SidebarInset,
   SidebarProvider,
 } from "@/components/ui/sidebar"
 
-import data from "./data.json"
 import { resources } from "@/lib/io"
 import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import { MDXRemote } from "next-mdx-remote/rsc";
-import { YouTubeEmbed } from "@next/third-parties/google";
-import { components } from "@/components/mdx";
 import { dirs } from "@/lib/consts";
-import { SiteFooter } from "@/components/site-footer";
 
-// Content for these pages will be fetched with getPost function.
-// This function is called at build time.
-// It returns the content of the post with the matching slug.
-// It also returns the slug itself, which Next.js will use to determine which page to render at build time.
-//For example, { props: { slug: "my-first-post", content: "..." } }
-async function getLearningResource({ slug }: { slug: string }) {  
-    const markdownFile = fs.readFileSync(
-    path.join(dirs.posts, slug + ".mdx"),
-    "utf-8"
-  );
-  const { data: frontMatter, content } = matter(markdownFile);
-  return {
-    frontMatter,
-    slug,
-    content,
-  };
-}
 
 // generateStaticParams generates static paths for blog posts.
 // This function is called at build time.
