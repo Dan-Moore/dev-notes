2bcc12b29b263e30e47f3b07eb55e3404e4977cd
Dan Moore <grep.daniel@gmail.com>
2025-05-14 14:57:50 -0400

    mono init

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,13 @@
+import { bar } from "./foo"
+import  Hi  from "../../markdown/posts/hi.mdx"
+
+export default function Page() {
+  console.log(bar());
+  return (
+    <div className="flex items-center justify-center min-h-svh">
+      <div className="flex flex-col items-center justify-center gap-4">
+      <Hi />
+      </div>
+    </div>
+  )
+}
